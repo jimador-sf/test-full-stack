1cd5c2dc29bd5923bd641212feae80342d593edb
jimador <james@dunnam.dev>
2020-07-01 01:50:36 -0400

    feat: Initial commit for frontend.

---
+++ frontend/dist/frontend.esm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// see: https://github.com/storybookjs/storybook/issues/9556
+
+var Thing = function Thing(_ref) {
+  var children = _ref.children;
+  return React.createElement("div", null, children || "the snozzberries taste like snozzberries");
+};
+
+export { Thing };
+//# sourceMappingURL=frontend.esm.js.map
