f48f353e80da8870bbfbe99e001b70662a9a4532
jimador-sf <james@dunnam.dev>
2020-07-02 16:13:38 -0400

    chore: docs and react testing lib

--- apps/user-management-api/src/resolver/UserResolver.ts
+++ apps/user-management-api/src/resolver/UserResolver.ts
@@ -2,6 +2,9 @@ import { Resolver, Query, Arg, Mutation } from 'type-graphql';
 import { User, UserInput } from '@test-full-stack/user-domain';
 import { userRepository } from '../repository/UserRepository';
 
+/**
+ * type-graphql {@link Resolver} for {@link User} data
+ */
 @Resolver(User)
 export default class ProductResolver {
   constructor() {
@@ -13,11 +16,22 @@ export default class ProductResolver {
     return result;
   }
 
+  @Query(_ => User)
+  async findOne(@Arg("id") id: string) {
+    const result = await userRepository.findOne(id)
+    return result
+  }
+
   @Mutation(_ => User)
   async addUser(
     @Arg("user") user: UserInput,
   ): Promise<User> {
     const result = await userRepository.save(user);
     return result;
+  }
+
+  @Mutation()
+  async deleteUser(@Arg("id") id: string) {
+    await userRepository.delete(id);
   }
 }
